dbeb203fd041f69a3d3b615adc399dab3f6c50da
Shashwat Dewangan <shashwatdewangan412@gmail.com>
2025-07-24 18:59:57 +0530

    map

--- City/client/src/pages/Landing.jsx
+++ City/client/src/pages/Landing.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 const Landing = () => {
   return (
     <div className='landing-main'>
-    <h1>Landing Page</h1>
-    <p>Hello and welcome!</p>
+    <h1>Welcome to City Pulse</h1>
+    <p>where streets glow</p>
     <Link to="/login" className="landing-login-button">Login</Link>
     <Link to="/register" className="landing-register-button">Register</Link>
   </div>
