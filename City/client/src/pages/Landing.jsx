de968bf30f0e44a3eaa3bb7c2a01bd592894ad66
Shashwat Dewangan <shashwatdewangan412@gmail.com>
2025-07-24 19:12:59 +0530

    map

--- City/client/src/pages/Landing.jsx
+++ City/client/src/pages/Landing.jsx
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import "../styles/Landing.css";
-// import { Link } from 'react-router-dom';
-
-// const Landing = () => {
-//   return (
-//     <div className='landing-main'>
-//     <h1>Welcome to City Pulse</h1>
-//     <p>where streets glow</p>
-//     <Link to="/login" className="landing-login-button">Login</Link>
-//     <Link to="/register" className="landing-register-button">Register</Link>
-//   </div>
-//   )
-// }
-
-// export default Landing
-
-
-
-
 import React from 'react'
 import "../styles/Landing.css";
 import { Link } from 'react-router-dom';
@@ -25,16 +5,36 @@ import { Link } from 'react-router-dom';
 const Landing = () => {
   return (
     <div className='landing-main'>
-    <div className="animated-text-container">
-      <div className="animated-text">
-        Welcome to CityPulse – Empowering Communities.
-      </div>
-      <Link to="/login" className="landing-login-button">Login</Link>
+    <h1>Welcome to City Pulse</h1>
+    <p>where streets glow</p>
+    <Link to="/login" className="landing-login-button">Login</Link>
     <Link to="/register" className="landing-register-button">Register</Link>
-    </div>
-    
   </div>
   )
 }
 
 export default Landing
+
+
+
+
+// import React from 'react'
+// import "../styles/Landing.css";
+// import { Link } from 'react-router-dom';
+
+// const Landing = () => {
+//   return (
+//     <div className='landing-main'>
+//     <div className="animated-text-container">
+//       <div className="animated-text">
+//         Welcome to CityPulse – Empowering Communities.
+//       </div>
+//       <Link to="/login" className="landing-login-button">Login</Link>
+//     <Link to="/register" className="landing-register-button">Register</Link>
+//     </div>
+    
+//   </div>
+//   )
+// }
+
+// export default Landing
