b99c18694ddad7fdab03771d8069cd126ed05179
Shashwat Dewangan <shashwatdewangan412@gmail.com>
2025-07-24 19:09:28 +0530

    map

--- City/client/src/pages/Landing.jsx
+++ City/client/src/pages/Landing.jsx
@@ -1,3 +1,23 @@
+// import React from 'react'
+// import "../styles/Landing.css";
+// import { Link } from 'react-router-dom';
+
+// const Landing = () => {
+//   return (
+//     <div className='landing-main'>
+//     <h1>Welcome to City Pulse</h1>
+//     <p>where streets glow</p>
+//     <Link to="/login" className="landing-login-button">Login</Link>
+//     <Link to="/register" className="landing-register-button">Register</Link>
+//   </div>
+//   )
+// }
+
+// export default Landing
+
+
+
+
 import React from 'react'
 import "../styles/Landing.css";
 import { Link } from 'react-router-dom';
@@ -5,8 +25,11 @@ import { Link } from 'react-router-dom';
 const Landing = () => {
   return (
     <div className='landing-main'>
-    <h1>Welcome to City Pulse</h1>
-    <p>where streets glow</p>
+    <div className="animated-text-container">
+      <div className="animated-text">
+        Welcome to CityPulse – Empowering Communities.
+      </div>
+    </div>
     <Link to="/login" className="landing-login-button">Login</Link>
     <Link to="/register" className="landing-register-button">Register</Link>
   </div>
